9e1f4029e45e35839d7d7a9b8db45e6501d7642a
xamanDawud <ahmeddaudking100@gmail.com>
2022-08-04 16:06:35 +0600

    first commit

---
+++ average.js
@@ -0,0 +1,8 @@
+var bangla = 88;
+var english = 55;
+var math = 55;
+var history = 78;
+
+var total = bangla + english + math + history;
+var average = total / 4;
+console.log(average);
---
+++ variable.js
@@ -0,0 +1,9 @@
+var name = "Xaman  Dawud";
+var age = 23;
+var distric = "Sylhet";
+
+console.log(name, age, distric);
+
+var isHappy = true;
+var stealMoney = false;
+console.log(isHappy);
